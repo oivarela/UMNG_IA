aeafa927f4f88669663b4eddc8eabab2efa96a4f
Oscar Ivan Varela Velez <oivarela@Mac.bbrouter>
2026-09-23 21:45:35 -0500

    Limpieza y estructura correcta del patrón MVC en src/

--- src/models/src/views/src/controllers/src/main.py
+++
@@ -1,7 +0,0 @@
-from src.models.search_problem import SearchProblemModel
-from src.controllers.agent_controller import AgentController
-
-if __name__ == "__main__":
-    problem = SearchProblemModel(initial_state=(0, 0), goal_state=(3, 3))
-    controller = AgentController(problem)
-    controller.run()
